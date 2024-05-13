888b6d9c56440662074033c374c5c8a9aa01c3cb
nischhal-nub <nischalbh51@gmail.com>
2024-05-13 15:34:48 +0545

    added active link in sidebar

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import {sidebarLinks} from '../data/index'
-import { Link, NavLink, NavLinkProps } from 'react-router-dom'
+import React from 'react';
+import { sidebarLinks } from '../data/index';
+import { NavLink, NavLinkProps } from 'react-router-dom';
+import { cn } from '../utils';
 
 interface CustomNavLinkProps extends NavLinkProps {
     activeClassName?: string;
-  }
-  
+}
 
 const Sidebar = () => {
     return (
@@ -15,19 +15,36 @@ const Sidebar = () => {
                 <div className='page-urls font-urbanist'>
                     <ul className='mx-6 border-b-[1px] border-searchInputBorder'>
                         {sidebarLinks[0].links.map((item, i) => (
-                        <li key={i} className='text-textSecondary-200 cursor-pointer hover:text-accent transition py-2 first-of-type:pt-6 pb-2 last-of-type:pb-6'>
-                            <NavLink className='flex' activeClassName="active" to={item.url}><span className='text-2xl'>{item.icon}</span><span className='text-base ml-4'>{item.label}</span></NavLink>
-                            </li>))}
+                            <li key={i} className='text-textSecondary-200 cursor-pointer py-2 first-of-type:pt-6 pb-2 last-of-type:pb-6'>
+                                <NavLink className={({ isActive }) =>
+                                    [
+                                        'flex',
+                                        'hover:text-accent',
+                                        'transition',
+                                        isActive ? "text-accent" : "text-textSecondary-200",
+                                    ].join(" ")} to={item.url}>
+                                    <span className='text-2xl'>{item.icon}</span>
+                                    <span className='text-base ml-4'>{item.label}</span>
+                                </NavLink>
+                            </li>
+                        ))}
                     </ul>
                 </div>
                 <div className='personal-links font-urbanist'>
-                    <ul className='m-6 '>
-                    {sidebarLinks[1].links.map((item, i) => (<li key={i} className='flex text-textSecondary-200 cursor-pointer hover:text-accent transition first-of-type:pb-2 py-2 last-of-type:pt-2 last-of-type:pb-8'><span className='text-2xl'>{item.icon}</span><span className='text-base ml-4'>{item.label}</span></li>))}
+                    <ul className='m-6'>
+                        {sidebarLinks[1].links.map((item, i) => (
+                            <li key={i} className=' text-textSecondary-200 cursor-pointer hover:text-accent transition first-of-type:pb-2 py-2 last-of-type:pt-2 last-of-type:pb-8'>
+                                <NavLink to={item.url} className='flex'>
+                                    <span className='text-2xl'>{item.icon}</span>
+                                    <span className='text-base ml-4'>{item.label}</span>
+                                </NavLink>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </div>
         </>
-    )
-}
+    );
+};
 
-export default Sidebar
+export default Sidebar;
--- src/data/index.tsx
+++ src/data/index.tsx
@@ -31,10 +31,10 @@ export const sidebarLinks: SidebarLinks = [
         page:'page-urls',
         links : [
             {label:"Home",icon:<TiHome />,url:'/' },
-            {label:"Community",icon:<IoPeopleOutline/>,url:'/' },
+            {label:"Community",icon:<IoPeopleOutline/>,url:'/community' },
             {label:"Explore",icon:<MdOutlineExplore/>,url:'/explore' },
-            {label:"My store",icon:<MdOutlineStorefront/>,url:'/' },
-            {label:"Library",icon:<MdOutlineLibraryMusic/>,url:'/' },
+            {label:"My store",icon:<MdOutlineStorefront/>,url:'/mystore' },
+            {label:"Library",icon:<MdOutlineLibraryMusic/>,url:'/library' },
         ]
     },
     {
